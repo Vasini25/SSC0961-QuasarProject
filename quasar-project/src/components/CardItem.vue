beb307c93afd40d1f4cd1681e1b44df824ad22bc
joaosccpedro <67018082+joaosccpedro@users.noreply.github.com>
2023-07-22 18:03:30 -0300

    created popup expansion items

--- quasar-project/src/components/CardItem.vue
+++ quasar-project/src/components/CardItem.vue
@@ -1,14 +1,27 @@
 <template>
-    <q-card v-for="(corrida, index) in raceList" :key="index">
-      <q-card-section>
-        <q-item>
-          <q-item-section>
-            <q-item-label>{{ corrida.raceName }}</q-item-label>
-            <q-item-label caption>{{ corrida.Circuit.circuitName }} - {{ corrida.date }}</q-item-label>
-          </q-item-section>
-        </q-item>
-      </q-card-section>
-    </q-card>
+      <q-card v-for="(corrida, index) in raceList" :key="index">
+        <q-card-section>
+          <q-expansion-item-label>
+            {{ index + 1 }}. {{ corrida.raceName }} - {{ corrida.date }} 
+          </q-expansion-item-label>
+          <q-expansion-item popup caption="Clique para ver detalhes">
+            <q-item-section>
+              <q-item-label label>
+                Nome da Corrida: {{ corrida.raceName }}
+              </q-item-label>
+              <q-item-label label>
+                Nome do Circuito: {{ corrida.Circuit.circuitName }}
+              </q-item-label>
+              <q-item-label label>
+                Data da corrida: {{ corrida.date }}
+              </q-item-label>
+              <q-item-label label>
+                Horário da corrida: {{ corrida.time }} (UTC)
+              </q-item-label>
+            </q-item-section>
+          </q-expansion-item>
+        </q-card-section>
+      </q-card>
   </template>
   
   <script>
